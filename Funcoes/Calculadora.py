1280cb00307ea9caa8f4256550921da45d8bc36c
Arthur Henrique Santana dos Santos <e504639@corp.caixa.gov.br>
2023-03-01 12:31:51 -0300

    Inicio de calculadora

---
+++ Funcoes/Calculadora.py
@@ -0,0 +1,10 @@
+import numpy as np
+import Funcoes_matematicas as fm
+
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite o segundo valor: '))
+r = fm.adc(num1, num2)
+print (r)
+
+
+
---
+++ Funcoes/Funcoes_matematicas.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+def adc(a, b):
+    return a + b;
+
+
+# In[ ]:
+
+
+
+
